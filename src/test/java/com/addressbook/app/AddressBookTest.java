dbb8c6f8f100fc553b17bf81931ff1962425bcf6
Mohammad Hamza Ibrahim <mi469@live.mdx.ac.uk>
2024-05-17 00:16:53 +0100

    Completed test case 2 and 3:- After adding a contact, the latest contact in the arraylist<> should be the one you just added.

--- src/test/java/com/addressbook/app/AddressBookTest.java
+++ src/test/java/com/addressbook/app/AddressBookTest.java
@@ -1,11 +1,11 @@
 package com.addressbook.app;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -13,8 +13,19 @@ public class AddressBookTest {
     @Nested
     @DisplayName("AddressBook addContact tests")
     class AddressBookAddContactTests {
+        AddressBook addressBookTest;
+        Contact contactTest1;
+        Contact contactTest2;
+
+        @BeforeEach
+        void setUp() {
+            addressBookTest = new AddressBook();
+            contactTest1 = new Contact("Bob Tom", "Bob@test.com", "07777777777");
+            contactTest2 = new Contact("Adam Smith", "Adam@test.com", "07666666666");
+        }
+
         @Test
-        @DisplayName("addContact() should add an entry to the contact arraylist<> (in the 'AddressBook' class).")
+        @DisplayName("addContact() is called, the length of the contacts array should increase by 1 (in the 'AddressBook' class).")
         void testContactArrayLengthIncreasedByOne() {
             // Arrange
             AddressBook addressBook = new AddressBook();
@@ -25,10 +36,33 @@ public class AddressBookTest {
 
             // Act
             addressBook.addContact(contact);
-            System.out.println(contact.getId());
 
             // Assert
             assertEquals(1, addressBook.getContacts().size());
+        }
+
+        @Test
+        @DisplayName("addContact() should add an entry to the contact arraylist<> (in the 'AddressBook' class).")
+        void testContactInsertedToArrayLength() {
+            // Arrange
+            // Act
+            addressBookTest.addContact(contactTest1);
+
+            // Assert
+            assertEquals(1, addressBookTest.getContacts().size());
+            assertTrue(addressBookTest.getContacts().contains(contactTest1));
+        }
+
+        @Test
+        @DisplayName("After adding a contact, the latest contact in the arraylist<> should be the one you just added.")
+        void testContactAddedIsLastElementInArrayLength() {
+            // Arrange
+            // Act
+            addressBookTest.addContact(contactTest1);
+            addressBookTest.addContact(contactTest2);
+
+            // Assert
+            assertEquals(contactTest2, addressBookTest.getContacts().get(addressBookTest.getContacts().size() - 1));
         }
 
 
